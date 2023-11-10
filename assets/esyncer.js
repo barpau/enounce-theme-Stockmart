aa2406a8201bc15baf0ea42a90ac3d828b0f1f81
barapaulatova <bara.paulatova@datalite.cz>
2023-11-10 10:35:38 +0100

    Initial commit

---
+++ assets/esyncer.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  let scriptEle2 = document.createElement("script");
+  scriptEle2.setAttribute("src", "https://app.esyncer.com/js/fo.js?v=" + Math.floor(Math.random() * 100000000));
+  document.body.appendChild(scriptEle2);
+});
+
+
+
+
