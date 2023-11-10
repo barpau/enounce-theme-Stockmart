a7ae21340466c26bc7ffa8d4248db224bb46f88c
barapaulatova <bara.paulatova@datalite.cz>
2023-11-10 11:18:24 +0100

    testing getting tags

--- assets/esyncer.js
+++ assets/esyncer.js
@@ -2,7 +2,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let scriptEle2 = document.createElement("script");
   scriptEle2.setAttribute("src", "https://app.esyncer.com/js/fo.js?v=" + Math.floor(Math.random() * 100000000));
   document.body.appendChild(scriptEle2);
+  getProductTags();
 });
+
+
+
+function getProductTags() {
+// Get the URL of the parent window
+  var parentURL = window.opener.location.href;
+
+// Get the product handle from the parent window URL
+  var productHandle = parentURL.split("/").pop();
+
+  Shopify.getProduct(productHandle, function (product) {
+    var tags = product.tags.split(","); // Gets the product tags and splits them into an array
+
+// Do something with the obtained labels
+    console.log(tags); // Print the labels in the browser console
+  });
+}
 
 
 
